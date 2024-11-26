e3791a8c1121c1a3e2f7432030a5194a126b3726
Markus Nordin <markus.nordin@gmail.com>
2024-11-26 13:20:32 +0100

    Chapter 4: TypeORM
    The auto loading of schema is throwing me off a bit. Makes it harder to
    reason about the database structure. Guess I’m missing some sort of
    database schema output that’s also version controlled.
    
    We’ve touched a bit on migrations, but I suspect that those can clash
    really quick with the database tables/column being auto generated in
    development mode. Highly recommend to turn off auto generation from the
    start. It just creates headache.

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -9,22 +9,21 @@ import {
   Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
-import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
-import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get()
-  findAll(@Query() paginationQuery) {
-    // const { limit, offset } = paginationQuery;
-
-    return this.coffeesService.findAll();
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.coffeesService.findAll(paginationQuery);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
+  findOne(@Param('id') id: string) {
     return this.coffeesService.findOne(id);
   }
 
@@ -34,12 +33,12 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: number) {
+  remove(@Param('id') id: string) {
     return this.coffeesService.remove(id);
   }
 }
